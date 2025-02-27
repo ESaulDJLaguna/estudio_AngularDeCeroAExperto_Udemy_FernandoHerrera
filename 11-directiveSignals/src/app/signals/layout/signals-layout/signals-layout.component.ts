aa798753d7532251981cf967f9c287060062ef90
ESaulDJLaguna <esdjl_asse15@hotmail.com>
2025-02-27 13:36:40 -0600

    DELETE: project 11-directiveSignals

--- 11-directiveSignals/src/app/signals/layout/signals-layout/signals-layout.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-signals-layout',
-  templateUrl: './signals-layout.component.html',
-  styleUrl: './signals-layout.component.scss',
-})
-export class SignalsLayoutComponent {}
